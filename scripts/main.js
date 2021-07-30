46f1318577197b817e8cd91726351ceb6a041f77
DorianIacob <iacob.dorian.gr@gmail.com>
2021-07-30 14:50:34 +0300

    Add id to game table field. Add startGame & displayCharOnTable functions
    Add id to game table field.
    Improved interaction with the user (displaying or hiding some elements).
    Add first/not full version of startGame & displayCharOnTable functions.

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,9 @@
 let gameTable = [];
-let player1 = "";
-let player2 = "";
+let playCharacter = "";
 let instruction1 = document.getElementById("Instruction1");
 let instruction2 = document.getElementById("Instruction2");
 
-for(let i = 0; i < 3; ++i) {
+for (let i = 0; i < 3; ++i) {
 	gameTable[i] = [];
 	for(let j = 0; j < 3; ++j) {
 		gameTable[i][j] = "";
@@ -12,17 +11,39 @@ for(let i = 0; i < 3; ++i) {
 }
 
 let buttonX = document.getElementById("buttonX");
-buttonX.onclick = function () {setPlayersCharacters(buttonX.textContent)};
+buttonX.onclick = function () {setPlayCharacter(buttonX.textContent)};
 
 let button0 = document.getElementById("button0");
-button0.onclick = function () {setPlayersCharacters(button0.textContent)};
+button0.onclick = function () {setPlayCharacter(button0.textContent)};
 
-function setPlayersCharacters(character) {
+function setPlayCharacter(character) {
 	if (character === "X") {
-		player1 = "X";
-		player2 = "0"
+		playCharacter = "X";
 	} else {
-		player1 = "0";
-		player2 = "X";
+		playCharacter = "0";
 	}
+	instruction2.style.display = "block";
+	document.getElementById("startGame").style.display = "block";
+}
+
+let buttonStart = document.getElementById("buttonStart");
+buttonStart.onclick = startGame;
+
+function startGame() {
+	instruction1.textContent = "Is Player 1 turn";
+	document.getElementById("buttonsX0").style.display = "none";
+	instruction2.style.display = "none";
+	document.getElementById("startGame").style.display = "none";
+	document.getElementById("gameTable").style.display = "block";
+	for (let i = 1; i <= 3; ++i) {
+		for (let j = 1; j <= 3; ++j) {
+			document.getElementById("" + i + j).onclick = function() {displayCharOnTable("" + i + j)};
+		}
+	}
+}
+
+function displayCharOnTable(id) {
+	document.getElementById(id).textContent = playCharacter;
+	gameTable[id[0] - 1][id[1] - 1] = playCharacter;
+	document.getElementById(id).style.pointerEvents = "none";
 }
