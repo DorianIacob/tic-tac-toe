3d217e3022cb9aaea924b2996edb4ae3182a27d0
DorianIacob <iacob.dorian.gr@gmail.com>
2021-08-02 14:06:14 +0300

    Clean the code. Improved code's logic.
    Clean the code:
    - move some lines so that the variables are at the beginning and afer that the functions;
    - moved auxiliary functions at the bottom;
    - renamed function mainGame -> checkCharacter;
    - add some comments.
    Improved code's logic:
    - removed some unnecessary "break" statements;
    - introduce "gameFinish === 0" verification in some "for" loops;
    - change initialization of playCharacter variable from "" to "X" and as a result change the conditional verification of the variable in the setPlayCharacter function

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ let instruction1 = document.getElementById("Instruction1");
 let instruction2 = document.getElementById("Instruction2");
 let gameFinish = 0;
 let stepCounter = 0;
-let playCharacter = "";
+let playCharacter = "X";
 let gameTable = [];
 
 let buttonX = document.getElementById("buttonX");
@@ -11,18 +11,19 @@ buttonX.onclick = function () {setPlayCharacter(buttonX.textContent)};
 let button0 = document.getElementById("button0");
 button0.onclick = function () {setPlayCharacter(button0.textContent)};
 
+let buttonStart = document.getElementById("buttonStart");
+buttonStart.onclick = startGame;
+
+let buttonRestart = document.getElementById("buttonRestart");
+buttonRestart.onclick = restartGame;
+
 function setPlayCharacter(character) {
-	if (character === "X") {
-		playCharacter = "X";
-	} else {
+	if (character !== "X") {
 		playCharacter = "0";
 	}
 	instruction2.style.display = "block";
 	document.getElementById("startGame").style.display = "block";
 }
-
-let buttonStart = document.getElementById("buttonStart");
-buttonStart.onclick = startGame;
 
 function startGame() {
 	initializeGameTable();
@@ -33,48 +34,39 @@ function startGame() {
 	document.getElementById("gameTable").style.display = "block";
 	for (let i = 1; i <= 3; ++i) {
 		for (let j = 1; j <= 3; ++j) {
-			document.getElementById("" + i + j).onclick = function() {mainGame("" + i + j)};
+			document.getElementById("" + i + j).onclick = function() {checkCharacter("" + i + j)};
 		}
 	}
 }
 
-function initializeGameTable() {
-	for (let i = 0; i < 3; ++i) {
-		gameTable[i] = [];
-		for(let j = 0; j < 3; ++j) {
-			gameTable[i][j] = "";
-		}
-	}
-}
-
-function mainGame(id) {
+function checkCharacter(id) {
 	if (gameFinish === 0) {
+// Display the appropriate character (X or 0) in the clicked "div" container
 		document.getElementById(id).textContent = playCharacter;
-		gameTable[id[0] - 1][id[1] - 1] = playCharacter;
+// Disable all events of the just clicked "div" container
 		document.getElementById(id).style.pointerEvents = "none";
+		gameTable[id[0] - 1][id[1] - 1] = playCharacter;
 		++stepCounter;
 		if (stepCounter > 4) {
-//Check the main diagonal
+// Check the main diagonal
 			for (let i = 0, j = 0; i < 3; ++i, ++j) {
 				if ((gameTable[i][j] == "") || (gameTable[i][j] != playCharacter)) {
 					break;
 				} else if (i === 2) {
 					gameFinish = 1;
 					displayWinner();
-					break;
 				}
 			}
-//Check the secondary diagonal
-			for (let i = 0, j = 2; i < 3; ++i, --j) {
+// Check the secondary diagonal
+			for (let i = 0, j = 2; i < 3 && gameFinish === 0; ++i, --j) {
 				if (gameTable[i][j] == "" || gameTable[i][j] != playCharacter) {
 					break;
 				} else if (i === 2) {
 					gameFinish = 1;
 					displayWinner();
-					break;
 				}
 			}
-//Check rows & columns
+// Check rows & columns
 			for (let row = 0, column = 0; row < 3 && gameFinish === 0; ++row, ++column) {
 				for (let i = 0; i < 3; ++i) {
 					if (gameTable[i][column] == "" || gameTable[i][column] != playCharacter) {
@@ -82,24 +74,24 @@ function mainGame(id) {
 					} else if (i === 2) {
 						gameFinish = 1;
 						displayWinner();
-						break;
 					}
 				}
-				for (let j = 0; j < 3; ++j) {
+				for (let j = 0; j < 3 && gameFinish === 0; ++j) {
 					if (gameTable[row][j] == "" || gameTable[row][j] != playCharacter) {
 						break;
 					} else if (j === 2) {
 						gameFinish = 1;
 						displayWinner();
-						break;
 					}
 				}
 			}
 		}
+// The equality case
 		if (stepCounter === 9 && gameFinish === 0) {
 			instruction1.textContent = "Equality! No one won!"
 			document.getElementById("restartGame").style.display = "block";
 		}
+// Change the play character (mark) alternatively and diplay which player turn is
 		if (gameFinish === 0 && stepCounter !== 9) {
 			if (playCharacter === "X") {
 				playCharacter = "0";
@@ -119,18 +111,15 @@ function mainGame(id) {
 function displayWinner() {
 	if (stepCounter % 2 != 0) {
 		instruction1.textContent = "Player 1 you WON!!! Congratulations!!!"
-		instruction2.textContent = "Player 2 you have lost... Sorry..."
+		instruction2.textContent = "Player 2 you have lost... I'm sorry..."
 		instruction2.style.display = "block";
 	} else {
 		instruction1.textContent = "Player 2 you WON!!! Congratulations!!!"
-		instruction2.textContent = "Player 1 you have lost... Sorry..."
+		instruction2.textContent = "Player 1 you have lost... I'm sorry..."
 		instruction2.style.display = "block";
 	}
 	document.getElementById("restartGame").style.display = "block";
 }
-
-let buttonRestart = document.getElementById("buttonRestart");
-buttonRestart.onclick = restartGame;
 
 function restartGame() {
 	gameFinish = 0;
@@ -138,4 +127,13 @@ function restartGame() {
 	playCharacter = "";
 	initializeGameTable();
 	location.reload();
+}
+
+function initializeGameTable() {
+	for (let i = 0; i < 3; ++i) {
+		gameTable[i] = [];
+		for(let j = 0; j < 3; ++j) {
+			gameTable[i][j] = "";
+		}
+	}
 }
